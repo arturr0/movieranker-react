cf6ba185dd6c5b9066f029a197ec5a16f871a532
arturr0 <119508496+arturr0@users.noreply.github.com>
2025-02-22 20:49:55 +0100

    Update App.js

--- movierankerreact/src/App.js
+++ movierankerreact/src/App.js
@@ -14,7 +14,7 @@ function App() {
       if (email.trim() === '' || password.trim() === '') return;
 
       try {
-        const response = await fetch(`http://localhost:3000/auth/${endpoint}`, {
+        const response = await fetch(`https://movieranker-react/auth/${endpoint}`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email, password }),
